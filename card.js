f198619ad51d8290ff748767bcc9427cca2fe3a6
Luminus <92187350+Luminus9051@users.noreply.github.com>
2024-12-31 12:48:00 +0300

    Add files via upload

--- card.js
+++ card.js
@@ -1,22 +1,18 @@
-window.addEventListener('load', () =>{
-  document.body.style.display = "flex"
-})
+function anim_toggle() {
+  elements.forEach((element) => {
+    element.classList.toggle("paused");
+    element.removeEventListener("click", anim_toggle);
 
-const elements = Array.from(document.querySelectorAll("body > div > div"));
+    setTimeout(add, 1500, element);
+  });
+}
 
 function add(element) {
   element.addEventListener("click", anim_toggle);
   element.classList.toggle("paused");
 }
 
+const elements = Array.from(document.querySelectorAll(".card"));
 elements.forEach((element) => {
   add(element);
 });
-
-function anim_toggle() {
-  elements.forEach((element) => {
-    element.classList.toggle("paused");
-    element.removeEventListener("click", anim_toggle);
-    setTimeout(add, 3000, element);
-  });
-}
